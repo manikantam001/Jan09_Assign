a324e741ed29eb94ac6067177703a53d1d73e573
mani <manikanta72888.com>
2024-01-11 15:27:47 +0530

    new file added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("task to deploy application in jenkins");
+            Console.WriteLine("added new line");
+            Console.WriteLine("task to deploy application in jenkins");
+
+        }
+    }
+}
